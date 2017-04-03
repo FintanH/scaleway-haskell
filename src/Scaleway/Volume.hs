152a2535485db24c6b1451b148bace97816603cd
Fintan Halpenny <rossmanf@tcd.ie>
2017-04-03 21:58:30 +0100

    Added volume details

--- src/Scaleway/Volume.hs
+++ src/Scaleway/Volume.hs
@@ -1,17 +1,27 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module Scaleway.Volume
-    ( Volume(..)
+    ( Volume
     , VolumeId
     ) where
 
 import           Data.Aeson
-import           Data.Text    (Text)
+import           Data.Text             (Text)
 import           GHC.Generics
+import           Scaleway.Organization (OrganizationId)
+import           Scaleway.Server       (Server)
 
 newtype VolumeId = VolumeId Text deriving (Show, Eq, Generic)
 
-data Volume = Volume deriving (Show, Eq, Generic)
+data Volume = Volume {
+    volumeId     :: VolumeId
+  , volumeName   :: Text
+  , exportURI    :: Maybe Bool
+  , organization :: OrganizationId
+  , server       :: Maybe Server
+  , size         :: Int
+  , volumeType   :: Text
+} deriving (Show, Eq, Generic)
 
 instance FromJSON VolumeId
 instance ToJSON VolumeId
